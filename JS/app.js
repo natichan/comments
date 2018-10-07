182db2b2213374db333b0ea95d21606b37174911
Natalia <nataliamh.88@gmail.com>
2018-10-07 19:07:11 -0300

    comentar

---
+++ JS/app.js
@@ -0,0 +1,45 @@
+firebase.initializeApp({
+    apiKey: '"AIzaSyD6TBZeJztDVb8HedL7jonuv7hmPgTm7Fo"',
+    authDomain: 'comentariosreto.firebaseapp.com',
+    projectId: 'comentariosreto'
+  });
+  
+  // Initialize Cloud Firestore through Firebase
+  var db = firebase.firestore();
+function saveComment(){
+
+    const comment = document.getElementById('comment').value
+
+    db.collection("posts").add({
+        post: comment,
+    })
+    .then(function(docRef) {
+        // console.log("Document written with ID: ", docRef.id);
+        document.getElementById('comment').value = '';
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
+ // leer y obtener comentarios en tiempo real
+const post = document.getElementById('post');
+
+db.collection("posts")
+.onSnapshot((querySnapshot) => {
+    
+    post.innerHTML = '';
+    querySnapshot.forEach((doc) => {
+        // console.log(`${doc.id} => ${doc.data().post}`);
+        console.log(doc.data());
+
+        post.innerHTML += `<div>${doc.data().post}</div><button onclick="deletePost('${doc.id}')">Eliminar</button>`
+    });
+});
+
+function deletePost (id) {
+    db.collection("posts").doc(id).delete().then(function() {
+        // console.log("Document successfully deleted!");
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+}
