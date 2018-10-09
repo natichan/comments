309a4a8ffabc28852ca16577d850279eee5098c7
Natalia <nataliamh.88@gmail.com>
2018-10-09 17:54:24 -0300

    estilo y responsive

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true,
+      },
+      "rules": {
+        "no-named-as-default": 0,
+        "no-console": off,
+      },
 };
--- src/JS/app.js
+++ src/JS/app.js
@@ -1,49 +1,51 @@
 firebase.initializeApp({
-    apiKey: '"AIzaSyD6TBZeJztDVb8HedL7jonuv7hmPgTm7Fo"',
-    authDomain: 'comentariosreto.firebaseapp.com',
-    projectId: 'comentariosreto'
-  });
-  
-  // Initialize Cloud Firestore through Firebase
-  var db = firebase.firestore();
-function saveComment(){
+  apiKey: '"AIzaSyD6TBZeJztDVb8HedL7jonuv7hmPgTm7Fo"',
+  authDomain: 'comentariosreto.firebaseapp.com',
+  projectId: 'comentariosreto',
+});
+const db = firebase.firestore();
 
-    const comment = document.getElementById('comment').value
+// Initialize Cloud Firestore through Firebase
+function saveComment() {
+  const comment = document.getElementById('comment').value;
 
-    db.collection("posts").add({
-        post: comment,
+  db.collection('posts').add({
+    post: comment,
+  })
+    .then((docRef) => {
+      console.log('Document written with ID: ', docRef.id);
+      document.getElementById('comment').value = '';
     })
-    .then(function(docRef) {
-        // console.log("Document written with ID: ", docRef.id);
-        document.getElementById('comment').value = '';
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
+    .catch((error) => {
+      console.error('Error adding document: ', error);
     });
 }
- // leer y obtener comentarios en tiempo real
+// leer y obtener comentarios en tiempo real
 const post = document.getElementById('post');
 
-db.collection("posts")
-.onSnapshot((querySnapshot) => {
-    
+db.collection('posts')
+  .onSnapshot((querySnapshot) => {
     post.innerHTML = '';
     querySnapshot.forEach((doc) => {
-        // console.log(`${doc.id} => ${doc.data().post}`);
-        // console.log(doc.data());
+    // console.log(`${doc.id} => ${doc.data().post}`);
+    // console.log(doc.data());
 
-        post.innerHTML += `
+      post.innerHTML += `
         <div class="message">
             <p>${doc.data().post}</p>
             <button class="delete" onclick="deletePost('${doc.id}')">Eliminar</button>
-        </div>`
+        </div>`;
     });
-});
+  });
 
-function deletePost (id) {
-    db.collection("posts").doc(id).delete().then(function() {
-        // console.log("Document successfully deleted!");
-    }).catch(function(error) {
-        console.error("Error removing document: ", error);
+function deletePost(id) {
+  db.collection('posts').doc(id).delete().then(() => {
+  // console.log("Document successfully deleted!");
+  })
+    .catch((error) => {
+      console.error('Error removing document: ', error);
     });
 }
+
+deletePost();
+saveComment();
